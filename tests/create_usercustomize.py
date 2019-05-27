c4b4ae2a1390ece17641d5f9e8a7a2a64c90854c
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2019-05-27 11:43:30 +0900

    modified:   tests/create_usercustomize.py

--- tests/create_usercustomize.py
+++ tests/create_usercustomize.py
@@ -1,11 +1,15 @@
 import os
 import site
+import sys
 
-up = site.getusersitepackages()
-if not os.path.exists(up):
-    os.makedirs(up)
 
-with open(os.path.join(up, 'usercustomize.py'), 'w') as f:
-    f.write('''import sys
+if sys.version_info[0] == '2':
+    print(sys.version_info)
+    up = site.getusersitepackages()
+    if not os.path.exists(up):
+        os.makedirs(up)
+
+    with open(os.path.join(up, 'usercustomize.py'), 'w') as f:
+        f.write('''import sys
 sys.setdefaultencoding('utf-8')
 ''')
