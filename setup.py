6f1a668e33ec607b968d470e938643206018f7b5
nttcslab-sp-admin <49966740+nttcslab-sp-admin@users.noreply.github.com>
2019-12-10 09:23:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(name='kaldiio',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Multimedia :: Sound/Audio :: Analysis']
       )
