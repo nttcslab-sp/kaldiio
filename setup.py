af0971d12eab59ea2a348b918bf8e0e3245c997f
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2025-03-06 23:11:05 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
     ],
 )
