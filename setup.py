33270e4d32d460f43ae60e3d3c0edf9383a7ae29
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2025-03-06 23:08:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="kaldiio",
-    version="2.18.0",
+    version="2.18.1",
     description="Kaldi-ark loading and writing module",
     author="nttcslab-sp",
     # author_email='',
